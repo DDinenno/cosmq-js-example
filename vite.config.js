4dcec6138655fa671602698e9b0612b3187c8b7a
root <dom.dinenno@gmail.com>
2023-10-09 07:09:09 -0400

    swapped babel preset with new name and incremented version

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ export default {
   plugins: [
     babel({
       babelConfig: {
-        presets: ["babel-preset-cosmq-js"],
+        presets: ["babel-preset-cosmq"],
         plugins: ["transform-equal-chain"],
       },
     }),
   ],
   server: { port: 8080 },
   build: {
-    minify: false,
+    minify: true,
     sourcemap: true,
   },
 };
